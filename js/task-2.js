f670ed87cdc637cf96ebdd20fd1d3d9a10095625
za3DopoGo <143600017+za3DopoGo@users.noreply.github.com>
2023-11-28 15:44:39 +0200

    1

--- js/task-2.js
+++ js/task-2.js
@@ -1,44 +1,61 @@
-function calcAverageCalories(days) {
-    let arrOfCalories = [];
-    let sumOfCalories = 0;
-    for (let x of days) {
-        arrOfCalories.push(x.calories);
-    }
-    for (let i = 0; i < arrOfCalories.length; i++) {
-        sumOfCalories += arrOfCalories[i];
-        
-    }
-    if (arrOfCalories.length === 0) {
-        return 0;
-    }
-    return sumOfCalories / arrOfCalories.length;
-
+let getUsersWithFriend = (users, friendName) => {
+  let names = users.filter(user => user.friends.includes(friendName));
+  return names;
 }
 
-console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 3010 },
-      { day: "tuesday", calories: 3200 },
-      { day: "wednesday", calories: 3120 },
-      { day: "thursday", calories: 2900 },
-      { day: "friday", calories: 3450 },
-      { day: "saturday", calories: 3280 },
-      { day: "sunday", calories: 3300 }
-    ])
-  ); // 3180
-  
-  console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 2040 },
-      { day: "tuesday", calories: 2270 },
-      { day: "wednesday", calories: 2420 },
-      { day: "thursday", calories: 1900 },
-      { day: "friday", calories: 2370 },
-      { day: "saturday", calories: 2280 },
-      { day: "sunday", calories: 2610 }
-    ])
-  ); // 2270
-  
-  console.log(
-    calcAverageCalories([])
-  ); // 0
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"]
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"]
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"]
+  }
+];
+
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
---
+++ js/task-4.js
@@ -0,0 +1,49 @@
+let getTotalBalanceByGender = (users, gender) => {
+    let totalBalance = 0;
+    let arrOfBalance = users.filter(user => {return user.gender === gender})
+                            .map(user => user.balance)
+                            .forEach(balance => {totalBalance += balance});
+    return totalBalance;
+}
+
+const allUsers = [
+    {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+
+console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
